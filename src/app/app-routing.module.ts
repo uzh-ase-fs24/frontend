1324f30d9761c9e2cd984d88cd87de40e107835a
chrsBa <christoph.bachmann3@uzh.ch>
2024-03-08 18:09:42 +0100

    initial routing setup - issue 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
-    path: '',
-    loadChildren: () => import('./home/home.component').then(m => m.HomeComponent)
-  }
-  ,
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+    canActivate: [() => inject(AuthService).isAuthenticated()],
+  },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.component').then(m => m.LoginComponent)
-  }
-  ,
+    loadChildren: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
   {
     path: 'sign-up',
-    loadChildren: () => import('./sign-up/sign-up.component').then(m => m.SignUpComponent)
-  }
-
+    loadChildren: () =>
+      import('./sign-up/sign-up.component').then((m) => m.SignUpComponent),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
