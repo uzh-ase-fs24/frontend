23a323d0d024f73379bde622c6ee918532dc7263
Jordi Küffer <jordikueffer@gmail.com>
2024-04-05 20:37:13 +0200

    frontend #17: adds LocationRiddle Model in frontend

---
+++ src/app/model/location-riddle.ts
@@ -0,0 +1,11 @@
+export type LocationRiddle = {
+  locationRiddleId: string;
+  image: string;
+  comments: string[];
+};
+
+export type LocationRiddleDto = {
+  location_riddle_id: string;
+  image: string;
+  comments: string[];
+};
