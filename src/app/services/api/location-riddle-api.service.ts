a8ad7757e19f7e75ce59b211d3ef9c8b33cbfc83
Jordi Küffer <jordikueffer@gmail.com>
2024-04-08 14:35:52 +0200

    frontend #17: removes mock data from api service and improves api to backend

--- src/app/services/api/location-riddle-api.service.ts
+++ src/app/services/api/location-riddle-api.service.ts
@@ -1,9 +1,9 @@
 import {HttpClient} from '@angular/common/http';
 import {inject, Injectable} from '@angular/core';
-import {map, Observable, of} from 'rxjs';
-import {environment} from 'src/environments/environment';
+import {map, Observable} from 'rxjs';
 import {AuthService} from '../auth/auth.service';
 import {LocationRiddle, LocationRiddleDto} from "../../model/location-riddle";
+import {environment} from "../../../environments/environment";
 
 @Injectable()
 export class LocationRiddleApiService {
@@ -14,30 +14,15 @@ export class LocationRiddleApiService {
   }
 
   getLocationRiddles(): Observable<LocationRiddle[]> {
-    const mockData: LocationRiddle[] = [
-      {
-        locationRiddleId: 'mock_id_1',
-        comments: ['mock_comments_1'],
-        image: 'https://ionicframework.com/docs/img/demos/card-media.png'
-      },
-      {
-        locationRiddleId: 'mock_id_2',
-        comments: ['mock_comments_2'],
-        image: 'mock_image_2'
-      },
-      // Add more mock data as needed
-    ];
-
-    // Use the 'of' function from RxJS to create an Observable from the mock data
-    return of(mockData);
-
     return this.http.get<LocationRiddleDto[]>(environment.api.url + '/location-riddles').pipe(
       map((dtos: LocationRiddleDto[]) => {
         return dtos.map(dto => {
           return {
             locationRiddleId: dto.location_riddle_id,
+            userId: dto.user_id,
             comments: dto.comments,
-            image: dto.image
+            locationRiddleImage: dto.location_riddle_image,
+            createdAt: dto.created_at
           }
         })
       })
