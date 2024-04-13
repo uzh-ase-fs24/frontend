9fcd7ce67c074d07a439481e3747c49660016c33
Jordi Küffer <jordikueffer@gmail.com>
2024-04-13 19:39:05 +0200

    frontend #17: adds map component with functionality to set markers

---
+++ src/app/home/tabs/feed/ui/location-riddle-post/map/map.component.ts
@@ -0,0 +1,89 @@
+import {Component, ElementRef, input, OnInit, output, ViewChild} from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import {Icon, Style} from 'ol/style';
+import {Coordinate} from 'ol/coordinate';
+
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+  standalone: true
+})
+export class MapComponent implements OnInit {
+  @ViewChild('map', {static: true}) mapElement?: ElementRef;
+  markerCoordinates = input<Coordinate>();
+  markerCoordinatesChange = output<Coordinate>();
+
+  map?: Map;
+  defaultMapCenter = [914135.8295099558, 5901532.510434296]; // central of europe
+
+  private vectorSource = new VectorSource();
+  private vectorLayer = new VectorLayer({
+    source: this.vectorSource,
+  });
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.map = new Map({
+      target: this.mapElement?.nativeElement,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        this.vectorLayer,
+      ],
+      controls: [],
+      view: new View({
+        center: this.markerCoordinates() || this.defaultMapCenter, // central of europe
+        zoom: this.markerCoordinates() ? 16 : 4,
+      }),
+    });
+
+    this.removeMapAttribution();
+
+    if (this.markerCoordinates()) this.addMarker(this.markerCoordinates()!);
+
+    this.map.on('click', (event) => {
+      this.addMarker(event.coordinate, true);
+    });
+  }
+
+  addMarker(coordinate: Coordinate, emit = false) {
+    this.vectorSource.clear();
+
+    const marker = new Feature({
+      geometry: new Point(coordinate),
+    });
+
+    marker.setStyle(
+      new Style({
+        image: new Icon({
+          anchor: [0.5, 1],
+          src: 'assets/icons/marker.png',
+        }),
+      })
+    );
+
+    this.vectorSource.addFeature(marker);
+    if (emit) this.markerCoordinatesChange.emit(coordinate);
+  }
+
+  removeMapAttribution() {
+    const attribution = document.getElementsByClassName('ol-attribution');
+    if (attribution.length > 0) {
+      attribution[0].remove();
+    }
+  }
+
+}
