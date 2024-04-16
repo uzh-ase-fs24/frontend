fd3a3d8b0d91bf93cca752e956e22ae39323a038
chrsBa <christoph.bachmann3@uzh.ch>
2024-04-16 16:17:37 +0200

    fix feed loading

--- src/app/home/tabs/feed/data-access/feed-state.service.ts
+++ src/app/home/tabs/feed/data-access/feed-state.service.ts
@@ -1,14 +1,15 @@
-import {computed, effect, inject, Injectable, signal} from '@angular/core';
-import {forkJoin, map, Subject, switchMap} from 'rxjs';
-import {User} from 'src/app/model/user';
-import {connect} from 'ngxtension/connect';
-import {LocationRiddle} from "../../../../model/location-riddle";
-import {LocationRiddleApiService} from "../../../../services/api/location-riddle-api.service";
-import {ProfileApiService} from "../../../../services/api/profile-api.service";
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { forkJoin, map, Subject, switchMap } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { connect } from 'ngxtension/connect';
+import { LocationRiddle } from '../../../../model/location-riddle';
+import { LocationRiddleApiService } from '../../../../services/api/location-riddle-api.service';
+import { ProfileApiService } from '../../../../services/api/profile-api.service';
 
 type FeedState = {
   locationRiddles: LocationRiddle[];
   users: User[];
+  loading: boolean;
 };
 
 @Injectable()
@@ -21,26 +22,35 @@ export class FeedStateService {
   private state = signal<FeedState>({
     locationRiddles: [],
     users: [],
+    loading: true,
   });
 
   // Selectors
   public locationRiddles = computed(() => this.state().locationRiddles);
   public users = computed(() => this.state().users);
+  public loading = computed(() => this.state().loading);
 
   // Action Sources (Subjects)
   public refresh = new Subject<void>();
 
   // Sources (Observables)
-  private locationRiddlesSource = this.locationRiddleApiService.getLocationRiddles()
+  private locationRiddlesSource =
+    this.locationRiddleApiService.getLocationRiddles();
   private usersSource = this.locationRiddleApiService.getLocationRiddles().pipe(
-    map((locationRiddles) => forkJoin(locationRiddles.map((locationRiddle) => this.profileApiService.getProfile(locationRiddle.userId || '')))),
-    switchMap((users) => users),
+    map((locationRiddles) =>
+      forkJoin(
+        locationRiddles.map((locationRiddle) =>
+          this.profileApiService.getProfile(locationRiddle.userId || '')
+        )
+      )
+    ),
+    switchMap((users) => users)
   );
   private refreshLocationRiddlesSource = this.refresh.pipe(
-    switchMap(() => this.locationRiddlesSource),
+    switchMap(() => this.locationRiddlesSource)
   );
   private refreshUsersSource = this.refresh.pipe(
-    switchMap(() => this.usersSource),
+    switchMap(() => this.usersSource)
   );
 
   constructor() {
@@ -48,6 +58,7 @@ export class FeedStateService {
     connect(this.state)
       .with(this.locationRiddlesSource, (state, locationRiddles) => ({
         locationRiddles: locationRiddles,
+        loading: false,
       }))
       .with(this.usersSource, (state, users) => ({
         users: users,
@@ -59,6 +70,6 @@ export class FeedStateService {
         users: users,
       }));
 
-    effect(() => console.info("Feed State Change: ", this.state()));
+    effect(() => console.info('Feed State Change: ', this.state()));
   }
 }
