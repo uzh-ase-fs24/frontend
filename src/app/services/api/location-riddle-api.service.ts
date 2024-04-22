5dc1075f58e2993d4c11d950cb1a5f37f643a5da
Jordi Küffer <jordikueffer@gmail.com>
2024-04-22 18:09:24 +0200

    frontend #29: adjusts location riddle model to backend changes

--- src/app/services/api/location-riddle-api.service.ts
+++ src/app/services/api/location-riddle-api.service.ts
@@ -1,35 +1,36 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
-import { LocationRiddle, LocationRiddleDto, LocationRiddlePostDto } from '../../model/location-riddle';
-import { AuthService } from '../auth/auth.service';
+import {HttpClient} from '@angular/common/http';
+import {inject, Injectable} from '@angular/core';
+import {map, Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {LocationRiddle, LocationRiddleDto, LocationRiddlePostDto} from '../../model/location-riddle';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class LocationRiddleApiService {
-	http = inject(HttpClient);
-	auth = inject(AuthService);
+  http = inject(HttpClient);
+  auth = inject(AuthService);
 
-	constructor() {}
+  constructor() {
+  }
 
-	getLocationRiddles(): Observable<LocationRiddle[]> {
-		return this.http.get<LocationRiddleDto[]>(environment.api.url + '/location-riddles').pipe(
-			map((dtos: LocationRiddleDto[]) => {
-				return dtos.map((dto) => {
-					return {
-						locationRiddleId: dto.location_riddle_id,
-						userId: dto.user_id,
-						comments: dto.comments,
-						locationRiddleImage: dto.location_riddle_image.image_base64,
-						createdAt: dto.created_at,
-						rating: dto.rating
-					};
-				});
-			})
-		);
-	}
+  getLocationRiddles(): Observable<LocationRiddle[]> {
+    return this.http.get<LocationRiddleDto[]>(environment.api.url + '/location-riddles').pipe(
+      map((dtos: LocationRiddleDto[]) => {
+        return dtos.map((dto) => {
+          return {
+            locationRiddleId: dto.location_riddle_id,
+            userId: dto.user_id,
+            comments: dto.comments,
+            locationRiddleImage: dto.image_base64,
+            createdAt: dto.created_at,
+            rating: dto.rating
+          };
+        });
+      })
+    );
+  }
 
-	postLocationRiddle(locationRiddle: LocationRiddlePostDto): Observable<void> {
-		return this.http.post<void>(environment.api.url + '/location-riddles', locationRiddle);
-	}
+  postLocationRiddle(locationRiddle: LocationRiddlePostDto): Observable<void> {
+    return this.http.post<void>(environment.api.url + '/location-riddles', locationRiddle);
+  }
 }
