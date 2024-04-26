7f529c9ad9b01d7d05e9efa59fb7bb2a53a07cf4
Jordi Küffer <jordikueffer@gmail.com>
2024-04-26 11:21:12 +0200

    frontend #29: add comments to location-riddle-post

--- src/app/home/tabs/feed/ui/location-riddle-post/location-riddle-post.component.ts
+++ src/app/home/tabs/feed/ui/location-riddle-post/location-riddle-post.component.ts
@@ -1,52 +1,58 @@
 import { CommonModule } from '@angular/common';
 import { Component, input, output, signal } from '@angular/core';
 import {
-  IonAvatar,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardTitle,
-  IonContent,
-  IonFab,
-  IonFabButton,
-  IonIcon,
-  IonImg,
-  IonItem,
-  IonLabel,
-  IonModal
+	IonAvatar,
+	IonButton,
+	IonCard,
+	IonCardContent,
+	IonCardHeader,
+	IonCardTitle,
+	IonContent,
+	IonFab,
+	IonFabButton,
+	IonIcon,
+	IonImg,
+	IonInput,
+	IonItem,
+	IonLabel,
+	IonModal
 } from '@ionic/angular/standalone';
 import { Coordinate } from 'ol/coordinate';
 import { MapComponent } from '../../../../../shared/map/map.component';
 import { RatingComponent } from './rating/rating.component';
+import { Comment } from '../../../../../model/location-riddle';
 
 @Component({
-  selector: 'app-location-riddle-post',
-  templateUrl: './location-riddle-post.component.html',
-  imports: [
-    IonImg,
-    IonIcon,
-    IonFab,
-    IonFabButton,
-    IonCardContent,
-    IonCardTitle,
-    IonCardHeader,
-    IonCard,
-    IonAvatar,
-    IonModal,
-    IonContent,
-    IonItem,
-    IonLabel,
-    CommonModule,
-    MapComponent,
-    RatingComponent
-  ],
-  styleUrls: ['./location-riddle-post.component.scss'],
-  standalone: true
+	selector: 'app-location-riddle-post',
+	templateUrl: './location-riddle-post.component.html',
+	imports: [
+		IonImg,
+		IonIcon,
+		IonFab,
+		IonFabButton,
+		IonCardContent,
+		IonCardTitle,
+		IonCardHeader,
+		IonCard,
+		IonAvatar,
+		IonModal,
+		IonContent,
+		IonInput,
+		IonItem,
+		IonLabel,
+		IonButton,
+		CommonModule,
+		MapComponent,
+		RatingComponent
+	],
+	styleUrls: ['./location-riddle-post.component.scss'],
+	standalone: true
 })
 export class LocationRiddlePostComponent {
 	username = input<string>();
+	locationRiddleId = input<string>();
 	locationRiddleImage = input<string>();
-	comments = input<string[]>();
+	comments = input<Comment[]>();
 	rating = input<number>();
 	createdAt = input<number>();
 
