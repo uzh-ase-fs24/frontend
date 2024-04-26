df3410541887356f238f455dc64b3efea5a5676a
chrsBa <christoph.bachmann3@uzh.ch>
2024-04-26 16:12:09 +0200

    fix import merge conflict

--- src/app/home/tabs/feed/ui/location-riddle-post/location-riddle-post.component.ts
+++ src/app/home/tabs/feed/ui/location-riddle-post/location-riddle-post.component.ts
@@ -17,7 +17,6 @@ import { Coordinate } from 'ol/coordinate';
 import { LocationRiddle } from 'src/app/model/location-riddle';
 import { MapComponent } from '../../../../../shared/map/map.component';
 import { RatingComponent } from './rating/rating.component';
-import { LocationRiddle } from 'src/app/model/location-riddle';
 
 @Component({
 	selector: 'app-location-riddle-post',
