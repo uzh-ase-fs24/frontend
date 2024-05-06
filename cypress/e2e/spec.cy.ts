973a360b6251ed5946b8832406ef23924949b00e
larsuzh <lars.boesch@uzh.ch>
2024-05-06 16:05:37 +0200

    Revert "frontend #52: change icon for pwa"
    This reverts commit d986fec6dcd88201b821516feaf0fee4839dc6c3.

--- cypress/e2e/spec.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('My First Test', () => {
-	it('Visits the initial project page', () => {
-		cy.login();
-		cy.visit('/home');
-		cy.visit('/home/profile');
-		cy.contains('Login');
-	});
-});
