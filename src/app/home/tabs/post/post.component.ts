26683d48457568805bfe1518e6dbdbf26439aac7
Jordi Küffer <jordikueffer@gmail.com>
2024-05-07 21:59:48 +0200

    frontend #66: make camera tab open the camera by default and navigate back to home after exit

--- src/app/home/tabs/post/post.component.ts
+++ src/app/home/tabs/post/post.component.ts
@@ -1,26 +1,60 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Camera, CameraResultType } from '@capacitor/camera';
-import { IonButton, IonIcon, IonList, IonItem, IonSelect, IonSelectOption, IonPopover, IonContent } from '@ionic/angular/standalone';
+import {
+	IonButton,
+	IonContent,
+	IonIcon,
+	IonItem,
+	IonList,
+	IonPopover,
+	IonSelect,
+	IonSelectOption
+} from '@ionic/angular/standalone';
 import { MapComponent } from '../../../shared/map/map.component';
 import { Arenas } from '../../../model/arenas';
 import { PostStateService } from './data-access/post-state.service';
+import { Router } from '@angular/router';
+import { ToastService } from '../../../services/toast.service';
 
 @Component({
 	selector: 'app-post',
 	templateUrl: './post.component.html',
 	styleUrls: ['./post.component.scss'],
-	providers: [PostStateService],
+	providers: [PostStateService, ToastService],
 	standalone: true,
-	imports: [IonIcon, IonButton, MapComponent, IonSelect, IonList, IonItem, IonSelectOption, FormsModule, IonPopover, IonContent]
+	imports: [
+		IonIcon,
+		IonButton,
+		MapComponent,
+		IonSelect,
+		IonList,
+		IonItem,
+		IonSelectOption,
+		FormsModule,
+		IonPopover,
+		IonContent
+	]
 })
 export class PostComponent {
 	// Services
 	postState = inject(PostStateService);
-  arenas: string[] = [];
-  arenaOptions = Object.values(Arenas);
+	router = inject(Router);
+	toastService = inject(ToastService);
+
+	// Variables
+	arenas: string[] = [];
+	arenaOptions = Object.values(Arenas);
 
 	constructor() {}
+
+	async ionViewDidEnter() {
+		try {
+			await this.takePicture();
+		} catch (e) {
+			await this.router.navigate(['/home']);
+		}
+	}
 
 	uploadImage(event: any) {
 		const file = event.target?.files[0];
@@ -39,8 +73,15 @@ export class PostComponent {
 		this.postState.uploadImage.next(image.base64String || '');
 	}
 
-  completePost() {
-    this.postState.setArenas.next(this.arenas);
-    this.postState.completePost.next();
-  }
+	cancelPost() {
+		this.postState.cancelPost.next();
+		this.router.navigate(['/home']);
+	}
+
+	completePost() {
+		this.postState.setArenas.next(this.arenas);
+		this.postState.completePost.next();
+		this.toastService.success('Post uploaded successfully');
+		this.router.navigate(['/home/profile'], { state: { refresh: true } });
+	}
 }
