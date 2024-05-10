b270a2e1674f4a6304bd85d150baae66c67941e8
larsuzh <lars.boesch@uzh.ch>
2024-05-10 14:41:53 +0200

    frontend #66: fix unused imports

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,46 +1,37 @@
-import { test, expect, devices } from '@playwright/test';
-
+import { test } from '@playwright/test';
 
 const authFile1 = 'playwright/.auth/test-user-1.json';
 const authFile2 = 'playwright/.auth/test-user-2.json';
 const authFile3 = 'playwright/.auth/test-user-with-data.json';
 
-
 test.describe('authenticate-users', () => {
-  test('authenticate-user-1', async ({page}) => {
-    await page.goto('http://localhost:8100/');
-    await page.getByLabel('Username or email address*').fill('e2e-1@find-me.life');
-    await page.getByLabel('Password*').fill('Password123');
-    await page.getByRole('button', {name: 'Continue'}).click();
-    await page.getByRole('button').nth(1).click();
+	test('authenticate-user-1', async ({ page }) => {
+		await page.goto('http://localhost:8100/');
+		await page.getByLabel('Username or email address*').fill('e2e-1@find-me.life');
+		await page.getByLabel('Password*').fill('Password123');
+		await page.getByRole('button', { name: 'Continue' }).click();
+		await page.getByRole('button').nth(1).click();
 
-    await page.context().storageState({path: authFile1});
+		await page.context().storageState({ path: authFile1 });
+	});
 
-  })
+	test('authenticate-user-2', async ({ page }) => {
+		await page.goto('http://localhost:8100/');
+		await page.getByLabel('Username or email address*').fill('e2e-2@find-me.life');
+		await page.getByLabel('Password*').fill('Password123');
+		await page.getByRole('button', { name: 'Continue' }).click();
+		await page.getByRole('button').nth(1).click();
 
-  test('authenticate-user-2', async ({page}) => {
-    await page.goto('http://localhost:8100/');
-    await page.getByLabel('Username or email address*').fill('e2e-2@find-me.life');
-    await page.getByLabel('Password*').fill('Password123');
-    await page.getByRole('button', {name: 'Continue'}).click();
-    await page.getByRole('button').nth(1).click();
+		await page.context().storageState({ path: authFile2 });
+	});
 
-    await page.context().storageState({path: authFile2});
+	test('authenticate-user-with-data', async ({ page }) => {
+		await page.goto('http://localhost:8100/');
+		await page.getByLabel('Username or email address*').fill('valentin.hollenstein@uzh.ch');
+		await page.getByLabel('Password*').fill('Password123');
+		await page.getByRole('button', { name: 'Continue' }).click();
+		await page.getByRole('button').nth(1).click();
 
-  })
-
-  test('authenticate-user-with-data', async ({ page }) => {
-    await page.goto('http://localhost:8100/');
-    await page.getByLabel('Username or email address*').fill('valentin.hollenstein@uzh.ch');
-    await page.getByLabel('Password*').fill('Password123');
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByRole('button').nth(1).click();
-
-    await page.context().storageState({ path: authFile3 });
-
-  });
-
+		await page.context().storageState({ path: authFile3 });
+	});
 });
-
-
-
--- tests/test-follow-request.spec.ts
+++ tests/test-follow-request.spec.ts
@@ -1,62 +1,53 @@
 import { test, expect, devices } from '@playwright/test';
-import * as fs from "fs";
 
 test.use({
-  ...devices['iPhone 14 Pro'],
+	...devices['iPhone 14 Pro']
 });
 
 test.describe('follow-request-flow', () => {
-  test.describe('e2e-user-1', ()=> {
-    test.use({
-      storageState: 'playwright/.auth/test-user-1.json',
-    });
+	test.describe('e2e-user-1', () => {
+		test.use({
+			storageState: 'playwright/.auth/test-user-1.json'
+		});
 
-    // create a follow request
-    test('test-create-follow-request', async ({ page }) => {
-      await page.context().storageState()
-      // authenticate with user 1
-      await page.goto('http://localhost:8100/home/network');
-      await page.getByLabel('Add or Search friends').fill('e2e-user-2');
+		// create a follow request
+		test('test-create-follow-request', async ({ page }) => {
+			await page.context().storageState();
+			// authenticate with user 1
+			await page.goto('http://localhost:8100/home/network');
+			await page.getByLabel('Add or Search friends').fill('e2e-user-2');
 
-      // button to create follow request shall be visible
-      await expect(page.locator('ion-button').getByRole('button')).toBeVisible();
-      await page.locator('ion-item').filter({ hasText: 'e2e-user-2E2E-2 Test-User' }).getByRole('button').click();
+			// button to create follow request shall be visible
+			await expect(page.locator('ion-button').getByRole('button')).toBeVisible();
+			await page.locator('ion-item').filter({ hasText: 'e2e-user-2E2E-2 Test-User' }).getByRole('button').click();
 
-      // wait for confirmation message to pop up
-      // await expect(page.getByText('Follow request sent!')).toBeVisible();
-    });
-  })
+			// wait for confirmation message to pop up
+			// await expect(page.getByText('Follow request sent!')).toBeVisible();
+		});
+	});
 
-  test.describe('e2e-user-2', ()=> {
-    test.use({
-      storageState: 'playwright/.auth/test-user-2.json',
-    });
+	test.describe('e2e-user-2', () => {
+		test.use({
+			storageState: 'playwright/.auth/test-user-2.json'
+		});
 
-    // accept the follow request
-    test('test-accept-follow-request', async ({ page }) => {
-      await page.goto('http://localhost:8100/home/network');
+		// accept the follow request
+		test('test-accept-follow-request', async ({ page }) => {
+			await page.goto('http://localhost:8100/home/network');
 
-      // green tickbox should be visible
-      await expect(page.getByRole('button').first()).toBeVisible();
-      // click accept
-      await page.getByRole('button').first().click();
+			// green tickbox should be visible
+			await expect(page.getByRole('button').first()).toBeVisible();
+			// click accept
+			await page.getByRole('button').first().click();
 
-      // go to profile
-      await page.locator('#tab-button-profile svg').click();
-      // expect to have one follower
-      await expect(page.getByRole('heading', { name: '1' })).toBeVisible();
-      // click on followers
-      await page.getByRole('heading', { name: '1' }).click();
-      // there should be e2e-user-1
-      await expect(page.getByRole('heading', { name: 'e2e-user-1' })).toBeVisible();
-    });
-
-  })
-})
-
-
-
-
-
-
-
+			// go to profile
+			await page.locator('#tab-button-profile svg').click();
+			// expect to have one follower
+			await expect(page.getByRole('heading', { name: '1' })).toBeVisible();
+			// click on followers
+			await page.getByRole('heading', { name: '1' }).click();
+			// there should be e2e-user-1
+			await expect(page.getByRole('heading', { name: 'e2e-user-1' })).toBeVisible();
+		});
+	});
+});
